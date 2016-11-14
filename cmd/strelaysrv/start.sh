7035e2988201bd007457ca0fc4bb6a3f3b771bea
Jakob Borg <jakob@nym.se>
2016-11-14 22:45:28 +0100

    wip

---
+++ cmd/strelaysrv/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+app \
+	-listen="${LISTEN:-:443}" \
+	-provided-by="${PROVIDEDBY:-Syncthing Docker Image}" \
+	-global-rate="${GLOBALRATE:-10000}" \
+	-per-session-rate="${SESSIONRATE:-1000}"
+
